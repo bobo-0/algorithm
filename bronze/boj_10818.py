dc97738609eb486883d3c164a1d7f160974cddd5
bobo-0 <84318113+bobo-0@users.noreply.github.com>
2022-05-13 10:27:25 +0900

    Create boj_10818.py

---
+++ bronze/boj_10818.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+data = list(map(int,sys.stdin.readline().split()))
+print(min(data), max(data))
+
+
+"""
+n = input()
+data = list(map(int,input().split()))
+print(min(data),max(data))
+
+"""
